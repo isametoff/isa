ea37731659122dfc06fe7d2a0b86f14fc2f3185a
Vadim Isametov <isametoff@gmail.com>
2022-06-10 14:36:20 +0200

    Отправление письма с подтверждением учетной записи

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, HasFactory, SoftDeletes;
 
